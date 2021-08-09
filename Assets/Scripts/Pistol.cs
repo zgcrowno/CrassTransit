cd73d631dc5468daf70c65728a6edca41fe5209b
zgcrowno <zgcrowno@gmail.com>
2021-08-08 20:49:01 -0500

    Fix for pistol's awake method. Also updated build settings.

--- Assets/Scripts/Pistol.cs
+++ Assets/Scripts/Pistol.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Pistol : Gun
 {
+    protected override void Awake()
+    {
+        base.Awake();
+
+        m_pHudInfo = GameObject.Find("PistolInfo");
+        m_pReloadingImage = GameObject.Find("HUD").transform.Find("ReloadingImage").gameObject;
+        m_pShotsInClipText = m_pHudInfo.transform.Find("ShotsInClip").GetComponent<TextMeshProUGUI>();
+        m_pClipSizeText = m_pHudInfo.transform.Find("ClipSize").GetComponent<TextMeshProUGUI>();
+        m_pNumClipsText = m_pHudInfo.transform.Find("NumClips").GetComponent<TextMeshProUGUI>();
+    }
+
     public override void GunSpecificFire(Vector2 _fireDirection)
     {
         int layerMask = LayerMask.NameToLayer("Player");
